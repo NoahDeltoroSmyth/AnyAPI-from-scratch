6248b6a758dfec485de9257791c8e22de6d50b24
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-15 15:34:52 -0700

    Ran through whole process for getAll method with passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Movie = require('../lib/models/Movie');
 
 describe('AnyAPI-from-scratch routes', () => {
   beforeEach(() => {
@@ -10,5 +11,11 @@ describe('AnyAPI-from-scratch routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('gets a list of movies', async () => {
+    const expected = await Movie.findAll();
+    const res = await request(app).get('/api/v1/movies');
+    expect(res.body).toEqual(expected);
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE movies (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL
+    genre TEXT NOT NULL
+    duration INT NOT NULL
+    release_date INT NOT NULL
+);
+
+INSERT INTO
+movies (title, genre, duration, release_date);
+VALUES
+    ('The Pick of Destiny', 'comedy', 94, 2006),
